672e53b94ceb8329c6be3bb19f61b794e2a0baa9
viktoriialeiko <viktoriia.leiko@gmail.com>
2025-11-25 17:06:20 +0100

    implementation Effect interface with TransformationFixed, ArbitraryBasic, EffectOr and unit tests

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/Effect.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/Effect.java
@@ -14,22 +14,22 @@ public interface Effect {
      * Checks whether this effect can produce the given output resources
      * from the provided input resources using the available pollution.
      *
-     * @param input     the list of input resources
-     * @param output    the desired list of output resources
-     * @param pollution the amount of pollution available for use
-     * @return true if the effect can be applied, false otherwise
+     * @param input     the list of input resources.
+     * @param output    the desired list of output resources.
+     * @param pollution the amount of pollution available for use.
+     * @return true if the effect can be applied, false otherwise.
      */
     boolean check(List<Resource> input, List<Resource> output, int pollution);
 
     /**
-     * Indicates whether this effect provides an Assistance reward
+     * Indicates whether this effect provides an Assistance reward.
      */
     boolean hasAssistance();
 
     /**
      * Returns a string representation of the effect's current state.
      *
-     * @return a descriptive string of the effect
+     * @return a descriptive string of the effect.
      */
     String state();
 }
