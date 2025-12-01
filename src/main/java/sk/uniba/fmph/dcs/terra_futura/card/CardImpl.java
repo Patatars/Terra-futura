a3930b1040b007ef0ea6cf9b7b8e24344a3a2268
Your Name <you@example.com>
2025-12-01 16:45:01 +0100

    To save some changes

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/card/CardImpl.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/card/CardImpl.java
@@ -13,13 +13,14 @@ private final int pollutionSpaceL;
 private final Effect upperEffect;
 private final Effect lowerEffect;
 
-public CardImpl(ArrayList<Resource> resources, int pollutionSpaceL, Effect upperEffect, Effect lowerEffect) {
+public CardImpl(final ArrayList<Resource> resources, final int pollutionSpaceL,
+                final Effect upperEffect, final Effect lowerEffect) {
         this.upperEffect = upperEffect;
         this.lowerEffect = lowerEffect;
         this.resources = resources;
         this.pollutionSpaceL = pollutionSpaceL;
     }
-private int countOfResource(List<Resource>  resources, Resource resourceType) {
+private int countOfResource(final List<Resource>  resources, final Resource resourceType) {
     int count = 0;
     for (Resource res : resources) {
         if (res == resourceType) {
@@ -29,7 +30,7 @@ private int countOfResource(List<Resource>  resources, Resource resourceType) {
     return count;
 }
 
-private List<Resource> kindsOfResources(List<Resource> resources) {
+private List<Resource> kindsOfResources(final List<Resource> resources) {
     ArrayList<Resource> kinds = new ArrayList<>();
     for (Resource res : resources) {
         if (!kinds.contains(res)) {
@@ -39,7 +40,7 @@ private List<Resource> kindsOfResources(List<Resource> resources) {
     return kinds;
 }
 
-public boolean canGetResources(List<Resource> resources) {
+public boolean canGetResources(final List<Resource> resources) {
     if (resources.isEmpty()) {
         return false;
     }
@@ -57,35 +58,35 @@ public boolean canGetResources(List<Resource> resources) {
     return true;
 }
 
-public boolean canPutResources(List<Resource> resources) {
+public boolean canPutResources(final List<Resource> resources) {
     if (countOfResource(this.resources, Resource.POLLUTION) >= this.pollutionSpaceL) {
         return false;
     }
     return true;
 }
 
-public void getResources(List<Resource> resources) {
+public void getResources(final List<Resource> resources) {
     if (!canPutResources(resources)) {
         throw new IllegalArgumentException("Cannot get resources from this card.");
     }
     ArrayList<Resource> willBeRemoved = new ArrayList<>();
     for (Resource res : this.resources) {
-        if(resources.contains(res) && countOfResource(willBeRemoved, res) < countOfResource(resources, res)) {
+        if (resources.contains(res) && countOfResource(willBeRemoved, res) < countOfResource(resources, res)) {
             willBeRemoved.add(this.resources.remove(this.resources.indexOf(res)));
         }
     }
 }
 
-public void putResources(List<Resource> resources) {
-    if(!canPutResources(resources)) {
+public void putResources(final List<Resource> resources) {
+    if (!canPutResources(resources)) {
         throw new IllegalArgumentException("Cannot put resources on this card.");
     }
     this.resources.addAll(resources);
 }
 
     @Override
-    public boolean check(List<Resource> input, List<Resource> output, int pollution) {
-    if(upperEffect == null){
+    public boolean check(final List<Resource> input, final List<Resource> output, final int pollution) {
+    if (upperEffect == null) {
         return false;
     }
     int pollutionCount = countOfResource(this.resources, Resource.POLLUTION);
@@ -95,8 +96,9 @@ public void putResources(List<Resource> resources) {
     }
 
     @Override
-    public boolean checkLower(List<Resource> input, List<Resource> output, int pollution) {
-    if(lowerEffect == null) {
+    public boolean checkLower(final List<Resource> input, final List<Resource> output,
+                              final int pollution) {
+    if (lowerEffect == null) {
         return false;
     }
         int pollutionCount = countOfResource(this.resources, Resource.POLLUTION);
